ec399c20d6bde66d6aebba4ed8487e5be97d9caf
Leecarry16 <71058886+Leecarry16@users.noreply.github.com>
2022-02-11 23:03:04 +0900

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,26 @@
+#!C:\Users\user\AppData\Local\Programs\Python\Python310\python.exe
+##Copyright Leecarry16
+##Contact For kesm03122@gmail.com or Leecarry16 for Kakaotalk
+
+print("content-type: text/html; charset=utf-8\n")
+print()
+
+import cgi
+form = cgi.FieldStorage()
+
+print('''<!DOCTYPE html>
+<html>
+<head>
+    <title>Xi Random Gatcha</title>
+    <meta charset="utf-8">
+</head>
+<body>
+    <div style="text-align:center">
+    <p><h2>Welcome to the 'Xistory Random Gatcha' Site</h2></p>
+    <p><h2>Please Login for service</h2></p>
+
+
+
+</div>
+</body>
+</html>''')
---
+++ register.py
@@ -0,0 +1,28 @@
+#!C:\Users\user\AppData\Local\Programs\Python\Python310\python.exe
+##Copyright Leecarry16
+##Contact For kesm03122@gmail.com or Leecarry16 for Kakaotalk
+
+print("content-type: text/html; charset=utf-8\n")
+print()
+
+import cgi
+form = cgi.FieldStorage()
+
+print('''<!DOCTYPE html>
+<html>
+<head>
+    <title>XRG register</title>
+    <meta charset="utf-8">
+</head>
+<body style="background-color:#36393F;">
+<div style="color: whitesmoke;">
+    <div style="text-align:center">
+    <p><h2>Sign up program is not make</h2></p>
+</div>
+
+
+
+
+</div>
+</body>
+</html>''')
